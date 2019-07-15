85849d202756f6485365885dc91131c8928fe65a
imjackicle <imjackicle@gmail.com>
2019-07-16 08:01:33 +0900

    Update gui_tkinter_combo_template.py
    Update gui_tkinter_combo_template.py
    - add functions
    - add labels
    - add button

--- gui/tkinter/python/gui_tkinter_combo_template.py
+++ gui/tkinter/python/gui_tkinter_combo_template.py
@@ -27,9 +27,23 @@ import webbrowser as wb
 # ---------------------------------------
 # ---------------------------------------
 # --- Function definition
+def func1(list, selected_item_num):
+	print("This is func1. You have selected [ " + str(list[selected_item_num]) + " ] from combo list 1")
 
+def func2(list, selected_item_num):
+	print("This is func2. You have selected [ " + str(list[selected_item_num]) + " ] from combo list 2")
 
+def func3(list, selected_item_num):
+	print("This is func3. You have selected [ " + str(list[selected_item_num]) + " ] from combo list 3")
 
+def func4(list, selected_item_num):
+	print("This is func4. You have selected [ " + str(list[selected_item_num]) + " ] from combo list 4")
+
+def func5(list, selected_item_num):
+	print("This is func5. You have selected [ " + str(list[selected_item_num]) + " ] from combo list 5")
+
+def func6(submit1_value):
+	print("This is func6. " + submit1_value)
 
 # ---------------------------------------
 # ---------------------------------------
@@ -63,6 +77,8 @@ frame4 = ttk.Frame(root, padding=8)
 # -- frame 5
 frame5 = ttk.Frame(root, padding=8)
 
+# -- frame 6
+frame6 = ttk.Frame(root, padding=8)
 
 
 
@@ -81,7 +97,7 @@ cb1 = ttk.Combobox(frame1, textvariable=cb1_val, height=10, width=43)
 cb1['values'] = combo1_itmes
 frame1.grid()
 cb1.grid(row=1)
-cb1.bind('<<ComboboxSelected>>', lambda e:[select_ui_language(combo1_itmes, int(cb.current()))])
+cb1.bind('<<ComboboxSelected>>', lambda e:[func1(combo1_itmes, int(cb1.current()))])
 
 # - combo box2 / action list for selected workspace
 combo2_itmes = [1, 2, 3, 4]
@@ -92,7 +108,7 @@ cb2 = ttk.Combobox(frame2, textvariable=cb2_val, height=10, width=43)
 cb2['values'] = combo2_itmes
 frame2.grid()
 cb2.grid(row=1)
-cb2.bind('<<ComboboxSelected>>', lambda e:[select_ui_language(combo2_itmes, int(cb.current()))])
+cb2.bind('<<ComboboxSelected>>', lambda e:[fun2(combo2_itmes, int(cb2.current()))])
 
 
 # - combo box3 / channel list of selected workspace
@@ -104,7 +120,7 @@ cb3 = ttk.Combobox(frame3, textvariable=cb3_val, height=10, width=43)
 cb3['values'] = combo3_itmes
 frame3.grid()
 cb3.grid(row=1)
-cb3.bind('<<ComboboxSelected>>', lambda e:[select_ui_language(combo3_itmes, int(cb.current()))])
+cb3.bind('<<ComboboxSelected>>', lambda e:[func3(combo3_itmes, int(cb3.current()))])
 
 
 # - combo box4 / action list about selected workspace
@@ -116,7 +132,7 @@ cb4 = ttk.Combobox(frame4, textvariable=cb4_val, height=10, width=43)
 cb4['values'] = combo4_itmes
 frame4.grid()
 cb4.grid(row=1)
-cb4.bind('<<ComboboxSelected>>', lambda e:[select_ui_language(combo4_itmes, int(cb.current()))])
+cb4.bind('<<ComboboxSelected>>', lambda e:[func4(combo4_itmes, int(cb4.current()))])
 
 
 # - combo box5 / xxx
@@ -128,18 +144,36 @@ cb5 = ttk.Combobox(frame5, textvariable=cb5_val, height=10, width=43)
 cb5['values'] = combo5_itmes
 frame5.grid()
 cb5.grid(row=1)
-cb5.bind('<<ComboboxSelected>>', lambda e:[select_ui_language(combo5_itmes, int(cb.current()))])
+cb5.bind('<<ComboboxSelected>>', lambda e:[func5(combo5_itmes, int(cb5.current()))])
 
 
 # ---------------
 # --- label
 
+lb1 = Label(frame1, text="Label 1", bg="mistyrose", font=("Helvetica 14" ))
+frame1.grid(); lb1.grid(row=0, sticky=W)
+
+lb2 = Label(frame2, text="Label 2", bg="mistyrose", font=("Helvetica 14"))
+frame2.grid(); lb2.grid(row=0, sticky=W)
+
+lb3 = Label(frame3, text="Label 3", bg="mistyrose", font=("Helvetica 14"))
+frame3.grid(); lb3.grid(row=0, sticky=W)
+
+lb4 = Label(frame4, text="Label 4", bg="mistyrose", font=("Helvetica 14"))
+frame4.grid(); lb4.grid(row=0, sticky=W)
+
+lb5 = Label(frame5, text="Label 5", bg="mistyrose", font=("Helvetica 14"))
+frame5.grid(); lb5.grid(row=0, sticky=W)
 
 
 
 # ---------------
 # --- butoon
 
+submit1_value = "You have clicked SUBMIT 1 buton"
+
+b1 = Button(frame6, text="SUBMIT", command=lambda:[func6(submit1_value)])
+frame6.grid(); b1.grid(row=0, sticky=W)
 
 
 # ---------------------------------------
